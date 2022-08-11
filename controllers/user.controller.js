fffaf49bd62d5857276cb16a2033759c721cba17
Enrique Dick <iscjedb@gmail.com>
2022-08-11 14:47:39 -0500

    User refactored to use controller

---
+++ controllers/user.controller.js
@@ -0,0 +1,48 @@
+const bcrypt = require("bcrypt");
+const _ = require("lodash");
+const { User, validate, generateAuthToken } = require("../models/user");
+
+async function getMyUserData(req, res) {
+  const me = await User.findOne({
+    attributes: ["id", "name", "email", "image"],
+    where: { id: req.user.id },
+  });
+  res.send(me);
+}
+
+async function getAllUsers(req, res) {
+  const users = await User.findAll();
+  res.send(users);
+}
+
+async function createUser(req, res) {
+  const { error } = validate(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  let user = await User.findOne({ where: { email: req.body.email } });
+  if (user) return res.status(400).send("User already registered.");
+
+  user = new User(_.pick(req.body, ["name", "email", "password", "image"]));
+  const salt = await bcrypt.genSalt(10);
+  user.password = await bcrypt.hash(user.password, salt);
+  console.log(user.dataValues);
+  let userRes = await User.create({
+    name: user.dataValues.name,
+    email: user.dataValues.email,
+    password: user.password,
+    image: user.dataValues.image || null,
+  });
+
+  const token = generateAuthToken(userRes.id);
+
+  res
+    .status(201)
+    .header("x-auth-token", token)
+    .send(_.pick(userRes, ["id", "name", "email"]));
+}
+
+module.exports = {
+  getMyUserData,
+  getAllUsers,
+  createUser,
+};
--- routes/users.js
+++ routes/users.js
@@ -1,48 +1,15 @@
 const router = require("express").Router();
 const auth = require("../middlewares/auth");
-const bcrypt = require("bcrypt");
-const _ = require("lodash");
-const { User, validate, generateAuthToken } = require("../models/user");
+
+const userController = require("./../controllers/user.controller");
 
 //Get current user
-router.get("/me", auth, async (req, res) => {
-  const me = await User.findOne({
-    attributes: ["id", "name", "email", "image"],
-    where: { id: req.user.id },
-  });
-  res.send(me);
-});
+router.get("/me", auth, userController.getMyUserData);
 
 //Get All Users
-router.get("/", auth, async (req, res) => {
-  const users = await User.findAll();
-  res.send(users);
-});
+router.get("/", auth, userController.getAllUsers);
 
-router.post("/", async (req, res) => {
-  const { error } = validate(req.body);
-  if (error) return res.status(400).send(error.details[0].message);
-
-  let user = await User.findOne({ where: { email: req.body.email } });
-  if (user) return res.status(400).send("User already registered.");
-
-  user = new User(_.pick(req.body, ["name", "email", "password", "image"]));
-  const salt = await bcrypt.genSalt(10);
-  user.password = await bcrypt.hash(user.password, salt);
-  console.log(user.dataValues);
-  let userRes = await User.create({
-    name: user.dataValues.name,
-    email: user.dataValues.email,
-    password: user.password,
-    image: user.dataValues.image || null,
-  });
-
-  const token = generateAuthToken(userRes.id);
-
-  res
-    .status(201)
-    .header("x-auth-token", token)
-    .send(_.pick(userRes, ["id", "name", "email"]));
-});
+// Create a new User
+router.post("/", userController.createUser);
 
 module.exports = router;
