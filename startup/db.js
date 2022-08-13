5975df72ec484e236c57a528dda64cc31f8ab181
Enrique Dick <iscjedb@gmail.com>
2022-08-13 16:09:15 -0500

    DB Config refactored

--- startup/db.js
+++ startup/db.js
@@ -1,19 +1,25 @@
-const { Sequelize } = require('sequelize');
+const { Sequelize } = require("sequelize");
 
-console.log('HOST: ', process.env.MYSQL_HOST);
+console.log("HOST: ", process.env.DB_HOST);
 
-const sequelize = new Sequelize(process.env.DB_NAME || 'appdb', "root", process.env.DB_PASSWORD || '9c%7O?PHoj0O', {
-  dialect: 'mysql',
-  host: process.env.DB_HOST || 'localhost',
-  retry: {
-    match: [
-      Sequelize.ConnectionError,
-      Sequelize.ConnectionTimedOutError,
-      Sequelize.TimeoutError,
-      /Deadlock/i,
-      'SQLITE_BUSY'],
-    max: 3
+const sequelize = new Sequelize(
+  process.env.DB_NAME || "appdb",
+  process.env.DB_USER || "root",
+  process.env.DB_PASSWORD || "9c%7O?PHoj0O",
+  {
+    dialect: "mysql",
+    host: process.env.DB_HOST || "localhost",
+    retry: {
+      match: [
+        Sequelize.ConnectionError,
+        Sequelize.ConnectionTimedOutError,
+        Sequelize.TimeoutError,
+        /Deadlock/i,
+        "SQLITE_BUSY",
+      ],
+      max: 3,
+    },
   }
-});
+);
 
 module.exports = sequelize;
